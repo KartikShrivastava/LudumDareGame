944e9f4a7ac4f5a9fa5b395b94d6bdb7b136add7
KartikShrivastava <shrivastavakartik19@gmail.com>
2019-10-05 17:10:00 +0530

    Updated project files

--- LudumDareGame/src/Application.cpp
+++ LudumDareGame/src/Application.cpp
@@ -321,8 +321,6 @@ int main() {
 		GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
 		GLCall(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
- 
-
 		/*GLCall(glBindVertexArray(lampVao));
 
 		normalShader.Bind();
